c8a8922d5acf1e2899f7c91225588b06c12a564e
Paulo Andre Azevedo Quirino <quirinpa@gmail.com>
2025-09-11 13:33:44 +0100

    Time stability

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "../include/draw.h"
 #include <limits.h>
+#include <stdlib.h>
+#include <sys/time.h>
 
 typedef struct {
 	double time;
 } ctx_t;
+
+long long start_t;
+
+static long long
+timestamp(void)
+{
+	struct timeval te;
+	gettimeofday(&te, NULL); // get current time
+	return te.tv_sec * 1000000LL + te.tv_usec;
+}
 
 static void anime(uint8_t *color,
 		uint32_t x, uint32_t y,
@@ -26,6 +38,13 @@ void fb_render(backend_t *be, draw_lambda_t *lambda,
 		uint32_t x, uint32_t y,
 		uint32_t w, uint32_t h, void *ctx);
 
+inline static double dt_get() {
+	long long tick = timestamp();
+	double ret = (tick - start_t) / 1000000.0;
+	start_t = tick;
+	return ret;
+}
+
 int main() {
 	backend_t be = {
 		.init = fb_init,
@@ -36,12 +55,14 @@ int main() {
 	be.init(&be);
 	double t = 0;
 
+	start_t = timestamp();
+
 	while (t < 3) {
 		ctx_t ctx = { .time = t };
 
 		be.render(&be, anime, 0, 0,
 				be.width, be.height, &ctx);
-		t += 0.01;
+		t += dt_get();
 	}
 
 	while (t < 5) {
@@ -49,7 +70,7 @@ int main() {
 
 		be.render(&be, anime, 10, 10,
 				100, 100, &ctx);
-		t += 0.01;
+		t += dt_get();
 	}
 
 	uint32_t tw = be.width / 3, th = be.height / 3;
@@ -59,7 +80,7 @@ int main() {
 
 		be.render(&be, anime, tw, th,
 				tw, th, &ctx);
-		t += 0.01;
+		t += dt_get();
 	}
 
 	be.deinit(&be);
